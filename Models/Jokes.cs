ad18313a338b73c39ebc828326f3b6f2079c0bbc
Irina Jinar <irinajinar.simedru@gmail.com>
2023-05-30 13:31:49 +0300

    Add project files.

---
+++ Models/Jokes.cs
@@ -0,0 +1,9 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public int JokeQuestion { get; set; }
+        public int JokeAnswer { get; set; }
+    }
+}
